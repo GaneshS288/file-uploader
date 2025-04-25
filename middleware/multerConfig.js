f42eee15dd98d8bea71ec4fb934815c80a1ddc8d
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-04-25 10:27:43 +0530

    feat: add a file filter function that will skip any existing files

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 import fs from "node:fs/promises";
 import fsSync from "fs";
-
+import { getUserFileByName } from "../db/queries.js";
 
 const storage = multer.diskStorage({
   destination: async (req, file, done) => {
@@ -18,12 +18,21 @@ const storage = multer.diskStorage({
   },
 });
 
-
+async function fileFilter(req, file, done) {
+  const fileExists = await getUserFileByName(req.user.id, file.originalname);
+  if (fileExists) {
+    console.log(`skipped file ${file.originalname}`);
+    req.body.skippedFiles
+      ? req.body.skippedFiles.push(file)
+      : (req.body.skippedFiles = [file]);
+    done(null, false);
+  } else done(null, true);
+}
 
 const upload = multer({
   storage: storage,
   limits: { fileSize: 1024 * 1024 }, //10MB
+  fileFilter: fileFilter,
 });
-
 
 export { upload };
